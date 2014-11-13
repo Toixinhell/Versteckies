332fda716c5c8615db638c94ac73e85581631abf
Toixinhell <online@ggaweb.ch>
2014-11-13 19:39:07 +0100

    Game Class implemented

---
+++ server/Game.js
@@ -0,0 +1,378 @@
+var util = require("util"),					// Utility resources (logging, object inspection, etc)
+    io = require("socket.io");				// Socket.IO
+Player = require("./Player").Player;
+
+
+/**************************************************
+ ** VARIABLES STRAIGHT FROM HELL
+ **************************************************/
+var DEBUG = false; 	//Are you shure?
+
+/**
+ * Created by Hagbart on 13.11.2014.
+ */
+/**************************************************
+ ** GAME Game CLASS
+ **************************************************/
+var Game = function(socket) {
+
+    var gameSocket = socket,
+        id,
+        isFull = false,
+        started = false,
+        players = [];
+
+
+    //Functions
+function initGame() {
+    // Create an empty array to store players
+    setEventHandlers();
+
+
+}
+
+    /**************************************************
+     ** GAME EVENT HANDLERS
+     **************************************************/
+    var setEventHandlers = function() {
+
+        // gameSocket.IO
+        console.log('new Connection established');
+        gameSocket.sockets.on("connection", onSocketConnection );
+
+    };
+
+// New gameSocket connection
+    function onSocketConnection(client) {
+        console.log('Setting Handlers');
+        console.log("New player has connected: "+client.id);
+
+        // Listen for client disconnected
+        client.on("disconnect", onClientDisconnect);
+
+        // Listen for new player message
+        client.on("new player", onNewPlayer);
+
+        // Listen for move player message
+        client.on("move player", onMovePlayer);
+
+        // Listen for client update
+        client.on("update player active", onPlayerActiveUpdate);
+
+
+    };
+
+
+
+// Get Player Updates
+    function onPlayerActiveUpdate(data) {
+
+        console.log('player sent update');
+        //console.log(data);
+        updatePlayer(data.id, data.isActive);
+
+    };
+
+// gameSocket client has disconnected
+    function onClientDisconnect() {
+        util.log("Player has disconnected: "+this.id);
+
+        var removePlayer = playerById(this.id);
+        var wasCatcher = false;
+
+        // Player not found
+        if (!removePlayer) {
+            util.log("Player not found: "+this.id);
+
+        }
+        else{
+            wasCatcher = removePlayer.getIsCatcher();
+        }
+
+        // Remove player from players array
+        players.splice(players.indexOf(removePlayer), 1);
+
+        // Broadcast removed player to connected gameSocket clients
+        this.broadcast.emit("remove player", {id: this.id});
+
+        if(wasCatcher){
+            var rand = Math.floor(Math.random()*players.length)
+            if(rand) {
+                var randId = players[rand].id;
+                players[rand].setIsCatcher(true);
+                console.log(randId);
+
+                this.broadcast.emit("new catcher", {id: randId});
+            }
+        }
+
+    };
+
+// New player has joined
+    function onNewPlayer(data) {
+
+
+        if(players.length <= 3) {
+
+            // Create a new player
+            var newPlayer = new Player(data.x, data.y);
+            newPlayer.id = this.id;
+
+            if (!catcherDefined()) {
+                //here we set the first player to be the catcher
+                newPlayer.setIsCatcher(true);
+                this.emit("new catcher", {id: newPlayer.id});
+                console.log('First player set to Catcher ' + newPlayer.getIsCatcher());
+
+                //reset players
+                //players = [];
+            }
+
+
+            // Broadcast new player to connected gameSocket clients
+            this.broadcast.emit("new player", {
+                    id: newPlayer.id,
+                    x: newPlayer.getX(),
+                    y: newPlayer.getY(),
+                    isCatcher: newPlayer.getIsCatcher()}
+            );
+
+            // Send existing players to the new player
+            var i, existingPlayer;
+            for (i = 0; i < players.length; i++) {
+                existingPlayer = players[i];
+                this.emit("new player", {
+                    id: existingPlayer.id,
+                    x: existingPlayer.getX(),
+                    y: existingPlayer.getY(),
+                    isCatcher: existingPlayer.getIsCatcher()
+                });
+                //console.log(existingPlayer.getIsCatcher() + ' id: ' + existingPlayer.id);
+            };
+
+
+
+
+            // Add new player to the players array
+            players.push(newPlayer);
+        }
+
+        else{
+
+        }
+    };
+
+// Player has moved
+    function onMovePlayer(data) {
+
+        // Find player in array
+        var movePlayer = playerById(this.id);
+
+
+        // Player not found
+        if (!movePlayer) {
+            util.log("Player not found: "+this.id);
+            return;
+        };
+
+        // Update player position
+        movePlayer.setX(data.x);
+        movePlayer.setY(data.y);
+
+        //Check for collision
+        collisionDetect();
+
+
+
+        //Check if game is over (moves are the only thing changing)
+        if(countActive() == 1){
+            this.broadcast.emit("game over", {msg: 'you lost!'});
+
+            //Now also set the last player inactive
+            console.log('last player set inactive');
+
+            movePlayer.setIsCatcher(false);
+            movePlayer.setIsActive(false);
+
+            this.emit("game over", {msg: 'you won'});
+
+        }
+        else
+        {
+            // Broadcast updated position to connected gameSocket clients
+            this.broadcast.emit("move player", {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY()});
+        }
+    };
+
+
+    /**************************************************
+     ** GAME HELPER FUNCTIONS
+     **************************************************/
+// Find player by ID
+    function playerById(id) {
+        var i;
+        for (i = 0; i < players.length; i++) {
+            if (players[i].id == id)
+                return players[i];
+        };
+
+        return false;
+    };
+
+
+    function hitDetection(i, j) {
+        return checkCoordinates(players[i].getX(), players[i].getY(), players[j].getX(), players[j].getY())
+            && players[i].id != players[j].id
+            && players[i].getIsActive()
+            && players[j].getIsActive()
+            && (players[i].getIsCatcher() ||
+                players[j].getIsCatcher());
+    }
+
+//Detecting a collision of any players
+    function collisionDetect() {
+
+        if(DEBUG){
+            console.log('Known Players on Server:');
+            var h;
+            for (h = 0;h < players.length; h++) {
+                console.log(players[h].id);
+            };
+        }
+
+
+        var i;
+        var j;
+        for (i = 0; i < players.length; i++) {
+            if(DEBUG){
+
+                console.log('------------Colision Detection Player 1------------' );
+                console.log(' id: ' + players[i].id );
+                console.log('X: ' + players[i].getX() + ' Y: ' + players[i].getY());
+                console.log('Player active: ' + players[i].getIsActive());
+                console.log('Player catcher: ' + players[i].getIsCatcher());
+
+            }
+            for (j = 0; j < players.length ; j++) {
+
+                if(DEBUG){
+
+                    console.log('-------------------Player 2----------------------------' );
+                    console.log('Player 2 id: ' + players[j].id);
+                    console.log('X: ' + players[j].getX() + ' Y: ' + players[j].getY());
+                    console.log('Player ACTIVE: ' + players[j].getIsActive());
+                    console.log('Player catcher: ' + players[j].getIsCatcher());
+                    console.log('-----------------------------------------------' );
+
+                }
+
+
+                if (hitDetection(i, j)){
+
+                    console.log('treffer!!');
+
+
+
+                    // Collision! hold your hats!
+                    if (!players[i].getIsCatcher())
+                    {
+                        players[i].setIsActive(false);
+                    }
+                    if (!players[j].getIsCatcher())
+                    {
+                        players[j].setIsActive(false);
+                    }
+
+
+
+                    srvMsg({ status : 2, payload : 'test collision' });
+                    gameSocket.emit("collision", {id1: players[i].id, id2: players[j].id});
+                    break;
+                }
+            };
+        };
+
+    };
+
+
+//Checking the coordinates of two players with a tolerance
+    function checkCoordinates(p1x, p1y, p2x, p2y) {
+
+        var returnVal = false;
+        var tol = 20; // tolerance for collision detection
+
+
+        if (p1x < p2x + tol &&
+            p1x + tol > p2x &&
+            p1y < p2y + tol &&
+            tol + p1y > p2y) {
+            // collision detected!
+            returnVal = true;
+        }
+
+        return returnVal;
+    }
+
+    function countActive() {
+
+        var countActive = 0;
+
+        var h;
+        for (h = 0;h < players.length; h++) {
+
+            if(players[h].getIsActive())
+            {
+                countActive++;
+            }
+        };
+
+        return countActive;
+    }
+
+    function catcherDefined() {
+
+        var i;
+
+        for (i = 0; i < players.length; i++) {
+            if (players[i].getIsCatcher())
+                return true;
+        };
+        return false;
+    }
+
+
+    function updatePlayer(id, active) {
+        var i;
+        for (i = 0; i < players.length; i++) {
+            if (players[i].id == id)
+                players[i].setIsActive(active);
+            return true;
+        };
+        return false;
+    };
+
+    function srvMsg(data) {
+        /*
+         Message levels:
+         1 = info
+         2 = err
+         */
+        console.log(data);
+        gameSocket.emit("server message", {status: data.status, payload: data.payload});
+    };
+
+    initGame();
+
+    // Define which variables and methods can be accessed
+    return {
+        id:id,
+        initGame:initGame
+    }
+
+
+};
+
+// Export the Game class so you can use it in
+// other files by using require("Game").Game
+exports.Game = Game;
+
