fd0cc3ad5631f3dfde0995101a5a5fc46832d10d
Toixinhell <online@ggaweb.ch>
2014-12-02 17:12:13 +0100

    Commeted server.js

--- server/server.js
+++ server/server.js
@@ -1,54 +1,57 @@
-/**************************************************
-** NODE.JS REQUIREMENTS
-**************************************************/
-var util = require("util"),					// Utility resources (logging, object inspection, etc)
-	io = require("socket.io");				// Socket.IO
-	Player = require("./Player").Player;	// Player class
-    Game = require("./Game").Game;            // Game class
+/**
+ * This is the Server class, responsible for handling client connections.
+ *
+ * This class can be seen as the main one. It initialises the basic socket.io connections,
+ * all connected players and the actual games. The players can connect as long as the game is not full.
+ *
+ * TODO: Implement the possibility for more than one game at a time.
+ *
+ * @category   ServerSide
+ * @author     Lukas Stahel
+ * @author     Stefan Bacoks
+ * @license    THE BEER-WARE LICENSE (Revision 42)
+ */
 
-/**************************************************
-** GAME VARIABLES
-**************************************************/
-var socket;		// Socket controller
+// Node.js requirements
+var util = require("util");
+var io = require("socket.io");
 
-/**************************************************
-** VARIABLES STRAIGHT FROM HELL
-**************************************************/
+// Class dependencies
+Player = require("./Player").Player;	// Player class
+Game = require("./Game").Game;            // Game class
+
+// Server variables
+var socket;	// Socket controller
 var DEBUG = false; 	//Are you shure?
-	
-/**************************************************
-** GAME INITIALISATION
-**************************************************/
+
+/**
+ *
+ * Initiate the server and start waiting for players
+ *
+ * @param    object  $object The object to convert
+ * @return      array
+ *
+ */
 function init() {
-    // Create an empty array to store games
+    // all games will be in the array
     games = [];
 
-// Start the game socket listening to port 8000
+    // Start the game socket listening to port 8000
     socket = io.listen(8000);
 
+    // Create one MOCK game to simulate
     var game1 = new Game(socket);
+
     game1.id = 1;
     game1.initGame(socket);
 
+    // Adding the MOCK game to the array of games
     games.push(game1);
 
-    console.log(game1);
-
-
-
-
-
-	console.log('New Game Started');
-
+    console.log('New Game Started');
 };
 
-
-
-
-
-/**************************************************
-** RUN THE GAME
-**************************************************/
+// Run the server
 init();
 
 
